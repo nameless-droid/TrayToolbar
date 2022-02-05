5c6bb1d060f5f74aa9decd7e8e8de6cb30e74fff
BuildTools <unconfigured@null.spigotmc.org>
2022-02-05 19:05:51 +0100

    Changed Files from 09.11.2021 08:19

---
+++ ActionIconButton.xaml.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrayToolbar
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ActionIconButton : UserControl
+    {
+        public string Command { get; set; }
+        public bool LightMode { get; set; }
+
+
+        private Brush oldBg;
+
+        public ActionIconButton(string text)
+        {
+            InitializeComponent();
+
+            Text = text;
+            lbl.Content = text;
+
+            this.DataContext = this;
+            this.MouseEnter += UserControl1_MouseEnter;
+            this.MouseLeave += UserControl1_MouseLeave;
+
+            PreviewMouseLeftButtonUp += (sender, args) => OnClick();
+        }
+
+        public void UpdateLightDarkMode(bool light)
+        {
+            if (light == true)
+            {
+                lbl.Foreground = Brushes.Black;
+            }
+            else
+            {
+                lbl.Foreground = Brushes.White;
+            }
+            LightMode = light;
+        }
+
+        private void UserControl1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            /*
+            //#E5F3FF
+            //stackpanel.Background.Opacity = 0.6;
+            oldBg = stackpanel.Background;
+            //this.Background.Opacity = 0.5;
+            Color oldBGCol = ((System.Windows.Media.SolidColorBrush)(oldBg)).Color;
+            //Color col = ChangeLightness(new SolidColorBrush(oldBGCol).Color, 10);
+            //Color col = ChangeLightness(oldBGCol, 5);
+            Color col = ChangeLightness(oldBGCol, 2);
+            stackpanel.Background = new SolidColorBrush(col);
+            */
+            MouseOver();
+        }
+
+        private void MouseOver()
+        {
+            if (LightMode == true)
+            {
+                //#EEEEEE
+                stackpanel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+            }
+            else
+            {
+                //#2B2B2B
+                stackpanel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#414141"));
+            }
+        }
+
+        private void UserControl1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            /*
+            //stackpanel.Background.Opacity = 1;
+            //this.Background.Opacity = 1;
+
+            stackpanel.Background = oldBg;
+            */
+            MouseNotOver();
+        }
+
+        private void MouseNotOver()
+        {
+            if (LightMode == true)
+            {
+                //#FFFFFF
+                stackpanel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EEEEEE"));
+            }
+            else
+            {
+                //#414141
+                stackpanel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2B2B2B"));
+            }
+        }
+
+        private const int MinLightness = 1;
+        private const int MaxLightness = 10;
+        private const float MinLightnessCoef = 1f;
+        private const float MaxLightnessCoef = 0.4f;
+
+        public Color ChangeLightness(Color color, int lightness)
+        {
+            if (lightness < MinLightness)
+                lightness = MinLightness;
+            else if (lightness > MaxLightness)
+                lightness = MaxLightness;
+
+            float coef = MinLightnessCoef +
+              (
+                (lightness - MinLightness) *
+                  ((MaxLightnessCoef - MinLightnessCoef) / (MaxLightness - MinLightness))
+              );
+
+            return Color.FromArgb(color.A, (byte)(int)(color.R * coef), (byte)(int)(color.G * coef),
+                (byte)(int)(color.B * coef));
+        }
+
+        public Brushes background;
+
+        public ImageSource icon;
+
+        public string Icon
+        {
+            set
+            {
+                SetIconFromFile(value);
+                //lbl.Content = System.IO.Path.GetFileName(value);
+            }
+        }
+
+        public string BG
+        {
+            set
+            {
+                //stackpanel.Background = new SolidColorBrush(Color));
+                stackpanel.Background = (Brush)new System.Windows.Media.BrushConverter().ConvertFromString(value);
+            }
+        }
+
+        public void SetIconFromFile(string filePath)
+        {
+            //filePath.Normalize();
+
+            if (filePath.StartsWith("\\"))
+            {
+                ;
+            }
+
+            if (filePath.StartsWith("//"))
+            {
+                ;
+            }
+
+            if (filePath.StartsWith("\""))
+            {
+                ;
+                //filePath = filePath.Substring(2);
+                filePath = filePath.TrimStart(filePath[0]);
+            }
+
+            if (filePath.EndsWith("\""))
+            {
+                ;
+                //filePath = filePath.Substring(2);
+                filePath = filePath.TrimEnd(filePath[^1]);
+            }
+
+            System.Drawing.Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(filePath);
+            img.Source = ToImageSource(icon);
+        }
+
+        public ImageSource ToImageSource(System.Drawing.Icon icon)
+        {
+            ImageSource imageSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                icon.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            return imageSource;
+        }
+
+        public string Text { get; set; }
+
+        public void SetText(string value)
+        {
+
+        }
+
+        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent(
+            "Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ActionIconButton));
+
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        void RaiseClickEvent()
+        {
+            RoutedEventArgs newEventArgs = new RoutedEventArgs(ActionIconButton.ClickEvent);
+            RaiseEvent(newEventArgs);
+        }
+
+        void OnClick()
+        {
+            RaiseClickEvent();
+        }
+
+        private void ucontrol_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        /////
+
+        ///// <summary>
+        ///// Create a custom routed event by first registering a RoutedEventID
+        ///// This event uses the bubbling routing strategy
+        ///// see the web page https://msdn.microsoft.com/EN-US/library/vstudio/ms598898(v=vs.90).aspx 
+        ///// </summary>
+        //public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(UserControl1));
+        ///// <summary>
+        ///// Provide CLR accessors for the event Click OPButton 
+        ///// Adds a routed event handler for a specified routed event Click, adding the handler to the handler collection on the current element.
+        ///// </summary>
+        //public event RoutedEventHandler Click
+        //{
+        //    add { AddHandler(ClickEvent, value); }
+        //    remove { RemoveHandler(ClickEvent, value); }
+        //}
+        ///// <summary>
+        ///// This method raises the Click event 
+        ///// </summary>
+        //private void RaiseClickEvent()
+        //{
+        //    RoutedEventArgs newEventArgs = new RoutedEventArgs(UserControl1.ClickEvent);
+        //    RaiseEvent(newEventArgs);
+        //}
+        ///// <summary>
+        ///// For isPressed purposes we raise the event when the OPButton is clicked
+        ///// </summary>
+        //private void OnClick()
+        //{
+        //    RaiseClickEvent();
+        //}
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace TrayToolbar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void TrayIconClickCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //MessageBox.Show("The New command was invoked");
+            Hardcodet.Wpf.TaskbarNotification.Interop.Point p = myNotifyIcon.GetPopupTrayPosition();
+            //this.Left = p.X;
+
+            GetCursorPos(out var pt);
+            //pt.X = 0;
+
+            this.Left = pt.X - this.Width / 2;
+            this.Top = p.Y - this.Height;
+
+            this.Show();
+        }
+        #region dll imports
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+        // DON'T use System.Drawing.Point, the order of the fields in System.Drawing.Point isn't guaranteed to stay the same.
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+
+        }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //foreach (string item in Directory.GetFiles(@"E:\Users\Anwender\Desktop\⠀"))
+            foreach (string item in Directory.GetFiles(@"E:\Visual Studio Test\StylingOfStuff\bin\Debug\Test"))
+            {
+                string text = System.IO.Path.GetFileName(item);
+                CreateButtonAndAddToStackPanel(item, text);
+            }
+            ///////////
+            //////////
+            /////////
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("test.xml");
+            XmlNodeList? itemNodes = xmlDoc.SelectNodes("//buttons/button");
+            int index = 0;
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                XmlNode textNode = itemNode.SelectSingleNode("text");
+                XmlNode commandNode = itemNode.SelectSingleNode("command");
+                if ((textNode != null) && (commandNode != null))
+                {
+                    //Button btn = new Button();
+                    //btn.Content = textNode.InnerText;
+                    //btn.Click += Btn_Click;
+                    //btn.Tag = commandNode.InnerText;
+
+                    CreateButtonAndAddToStackPanel(commandNode.InnerText, textNode.InnerText);
+
+                    if (itemNode.Attributes["color"] != null)
+                    {
+                        //try
+                        //{
+                        //    btn.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(itemNode.Attributes["color"].Value));
+                        //}
+                        //catch (Exception) { }
+                    }
+                    if (itemNode.Attributes["background"] != null)
+                    {
+                        //try
+                        //{
+                        //    btn.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(itemNode.Attributes["background"].Value));
+                        //}
+                        //catch (Exception) { }
+                    }
+
+                    //stackPanel.Children.Add(btn);
+                    index++;
+                }
+            }
+        }
+
+
+        private void CreateButtonAndAddToStackPanel(string filePath, string text)
+        {
+            ActionIconButton actionIconButton = new(text);
+            actionIconButton.Icon = filePath;
+            //actionIconButton.Text = item;
+            actionIconButton.Command = filePath;
+            //actionIconButton.BG = "Black";
+            //actionIconButton.BG = "#2B2B2B";
+            actionIconButton.LightMode = false;
+            actionIconButton.Click += ActionIconButton_Click;
+            stackPanel.Children.Add(actionIconButton);
+        }
+
+        private async void ActionIconButton_Click(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            //Process.Start(((ActionIconButton)sender).Command);
+            //using (Process p = new Process())
+            //{
+            //    p.StartInfo = new ProcessStartInfo()
+            //    {
+            //        CreateNoWindow = true,
+            //        UseShellExecute = true,
+            //        Verb = "print",
+            //        //FileName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\doc.pdf"
+            //        FileName = "cmd.exe",
+            //        //Arguments = ((ActionIconButton)sender).Command,
+            //    };
+            //    p.Start();
+            //}
+            //Cursor = Cursors.AppStarting;
+
+            Mouse.OverrideCursor = Cursors.AppStarting;
+            try
+            {
+                // do stuff
+            }
+            finally
+            {
+                //Mouse.OverrideCursor = null;
+            }
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "cmd";
+            processStartInfo.Arguments = "/c \"" + ((ActionIconButton)sender).Command + "\"";
+            //processStartInfo.UseShellExecute = true;
+            //processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            Process.Start(processStartInfo);
+            //Process.Start("cmd");
+            //Cursor = Cursors.Arrow;
+            await Task.Delay(1000);
+            Mouse.OverrideCursor = null;
+        }
+    }
+}
