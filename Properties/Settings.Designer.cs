f44c853236d47a099c88c7554b5f8f6076f6e459
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 16:06:19 +0100

    new .net framework 4.8 project

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,42 +8,21 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TrayToolbar.Properties {
-    
-    
+
+namespace TrayToolbar.Properties
+{
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("E:\\Users\\Anwender\\Desktop\\⠀")]
-        public string Path {
-            get {
-                return ((string)(this["Path"]));
-            }
-            set {
-                this["Path"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool showOnlyFiles {
-            get {
-                return ((bool)(this["showOnlyFiles"]));
-            }
-            set {
-                this["showOnlyFiles"] = value;
             }
         }
     }
